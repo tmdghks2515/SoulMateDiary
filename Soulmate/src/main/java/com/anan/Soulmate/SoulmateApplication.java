a2c1e381d0a9b7339c1925d78a926ca9fbb2d927
tmdghks2515 <tmdghks2515@naver.com>
2021-03-31 17:52:37 +0900

    first commit

---
+++ Soulmate/src/main/java/com/anan/Soulmate/SoulmateApplication.java
@@ -0,0 +1,13 @@
+package com.anan.Soulmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulmateApplication.class, args);
+	}
+
+}
---
+++ Soulmate/src/test/java/com/anan/Soulmate/SoulmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anan.Soulmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
